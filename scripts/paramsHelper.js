e7e5d032b1689e00cba2db535865beaecdd4d0de
Anton Pronkin <Anton_Pronkin@epam.com>
2020-10-25 19:20:14 +0300

    Generate frame information

---
+++ scripts/paramsHelper.js
@@ -0,0 +1,28 @@
+function getUrlParams(url)
+{
+	let params = [];
+
+	let urlParts = url.split(/[&\?]/);
+	for (let i = 1; i < urlParts.length; i++)
+	{
+		addParam(params, urlParts[i]);
+    }
+    
+    function addParam(params, param) {
+        let paramParts = param.split(/[=#]/);
+        if (paramParts.length < 2)
+        {
+            return;
+        }
+	
+        let name = decodeURIComponent(paramParts[0]);
+        let value = decodeURIComponent(paramParts[1]);
+
+        params.push({
+            name: name,
+            value: value
+        });
+    }
+
+	return params;
+}
--- scripts/popup.js
+++ scripts/popup.js
@@ -19,8 +19,89 @@ function getFramesInfo(tabs) {
 }
 
 function generateFramesInfo(framesInfo) {
+    let contentInfo = createContentInfo(framesInfo);
+    let content = generateContent(contentInfo);
+
+    let container = document.body;
+    container.appendChild(content);
+}
+
+function createContentInfo(framesInfo) {
+    let contentInfo = [];
+
     for (const frameInfo of framesInfo) {
-        document.writeln(frameInfo.title);
-        document.writeln(frameInfo.url);
+        let contentFrameInfo = createContentFrameInfo(frameInfo);
+        if (contentFrameInfo.params.length)
+        {
+            contentInfo.push(contentFrameInfo);
+        }
     }
+
+    return contentInfo;
+}
+
+function createContentFrameInfo(frameInfo) {
+    return {
+        title: frameInfo.title,
+        params: getUrlParams(frameInfo.url)
+    };
+}
+
+
+function generateContent(framesInfo) {
+    let content = createBlock("content");
+
+    let frames = generateFrames(framesInfo);
+    content.appendChild(frames);
+
+    return content;
+}
+
+function generateFrames(framesInfo) {
+    let frames = createList("content__frames");
+
+    for (const frameInfo of framesInfo) {
+        let frame = generateFrameInfo(frameInfo);
+        frames.appendChild(frame);
+    }
+
+    return frames;
+}
+
+function generateFrameInfo(frameInfo) {
+    let frame = createListItem("content__frame");
+
+    let title = createBlock("content__frame-title", frameInfo.title);
+    frame.appendChild(title);
+
+    let params = generateFrameParams(frameInfo.params);
+    frame.appendChild(params);
+
+    return frame;
+}
+
+function generateFrameParams(paramsInfo) {
+    let params = createList("content__frame-params");
+
+    for (const paramInfo of paramsInfo) {
+        let param = generateFrameParam(paramInfo);
+        params.appendChild(param);
+    }
+
+    return params;
+}
+
+function generateFrameParam(paramInfo) {
+    let param = createListItem("content__frame-param");
+
+    let paramName = createBlock("content__param-name", paramInfo.name);
+    param.appendChild(paramName);
+
+    let paramValue = createInput("content__param-value", paramInfo.value);
+    param.appendChild(paramValue);
+
+    let copyButton = createBlock("content__copy-param-button", "Copy");
+    param.appendChild(copyButton);
+
+    return param;
 }
