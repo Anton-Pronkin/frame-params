679639fbc602cff1b2a6cdca83ea0e4f8b13807e
Anton Pronkin <Anton_Pronkin@epam.com>
2020-10-25 18:21:09 +0300

    Add jquery supporting

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", getFramesInfoForActiveTab);
+$(document).ready(getFramesInfoForActiveTab);
 
 function getFramesInfoForActiveTab() {
     const tabsQuery = { 
