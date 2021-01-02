ce2cf5d3be34d0d0fa9b5a998eecd46c2b27d235
Anton Pronkin <anton_pronkin@epam.com>
2021-01-02 11:27:42 +0300

    Add webpack with react builder

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+      popup: "./src/scripts/popup.js",
+      options: "./src/scripts/options.js"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, "app/scripts/"),
+    filename: "[name].min.js"
+  }
+};
