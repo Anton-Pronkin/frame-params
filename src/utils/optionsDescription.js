ad6ab22a99c329e258d1fb02c3295c3672149699
Anton Pronkin <anton_pronkin@epam.com>
2021-01-04 11:19:11 +0300

    Export option descriptions as separate module

---
+++ src/utils/optionsDescription.js
@@ -0,0 +1,16 @@
+import OptionManager from "./optionManager";
+
+export default [{
+        name: OptionManager.optionNames.paramsHighlighting,
+        caption: "Highlight names of params that equal \"id\" or ends with \"id\""
+    }, {
+        name: OptionManager.optionNames.paramsSorting,
+        caption: "Sort params alphabetically"
+    }, {
+        name: OptionManager.optionNames.emptyFramesHiding,
+        caption: "Hide frames that have no params"
+    }, {
+        name: OptionManager.optionNames.pageUrlDisplaying,
+        caption: "Show page URL instead of the page title"
+    }
+];
